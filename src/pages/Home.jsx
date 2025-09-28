097b55a9c8789f4a3032132865c0e1c92779cfb4
Ajay Mathew <ajay.02mathew@gmail.com>
2025-09-28 17:12:36 +0530

    feat: add Banner and Testimonial components to Home page for enhanced user engagement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../components/Hero'
 import CarCard from '../components/CarCard'
 import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <>
       <Hero />
      <FeaturedSection />
+     <Banner />
+     <Testimonial />
     </>
     
   )
