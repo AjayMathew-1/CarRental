4310e4ebb67eed460fef0d253150a65a447f4ba8
Ajay Mathew <ajay.02mathew@gmail.com>
2025-09-28 10:25:49 +0530

    feat: add FeaturedSection component and integrate it into Home page; implement Title component for better UI structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import CarCard from '../components/CarCard'
+import FeaturedSection from '../components/FeaturedSection'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <CarCard />
+     <FeaturedSection />
     </>
     
   )
